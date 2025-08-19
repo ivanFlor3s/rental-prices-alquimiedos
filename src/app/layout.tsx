b2c247c0c140c0296d472e3d88ef8b312810c9d7
Ivan Flores <ifloresmanc96@gmail.com>
2025-08-19 18:32:29 -0300

    feat: using default view transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { unstable_ViewTransition as ViewTransition } from 'react';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -27,7 +28,9 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <Navbar />
-                <main className="min-h-screen bg-gray-50">{children}</main>
+                <ViewTransition name="page">
+                    <main className="min-h-screen bg-gray-50">{children}</main>
+                </ViewTransition>
             </body>
         </html>
     );
