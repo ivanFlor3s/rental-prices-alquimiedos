d2c7763fc5a5dbfcb35fae80b17c43a0fac2b2ee
Ivan Flores <ifloresmanc96@gmail.com>
2025-09-11 21:55:57 -0300

    stadistics page

---
+++ src/app/estadisticas/page.tsx
@@ -0,0 +1,42 @@
+import EstadisticasChart from '@/components/reports/EstadisticasChart';
+import { MenuItem, SideMenu } from '@/components/SideMenu';
+const EstadisticasPage = () => {
+    const menu: MenuItem[] = [
+        { name: 'Introducción', link: '#introduccion' },
+        {
+            name: 'Alquileres',
+            link: '#alquileres',
+            children: [
+                { name: 'Progresión histórica', link: '#progresion-historica' },
+                { name: 'Por barrio', link: '#alquileres-por-barrio' },
+                { name: 'Por m²', link: '#alquileres-m2' },
+                { name: 'Por ambientes', link: '#alquileres-ambientes' },
+                { name: 'Con/ sin gastos', link: '#alquileres-gastos' },
+                { name: 'Ranking de barrios', link: '#ranking-barrios' },
+            ],
+        },
+        { name: 'Metodología', link: '#metodologia' },
+    ];
+    return (
+        <>
+            <div className="bg-purple-700 text-white p-4 mb-6">
+                <h1 className="text-lg font-bold text-center">Resultados analisis alquileres CABA</h1>
+            </div>
+            <div className="max-w-[1000px] mx-auto ">
+                <div className="grid grid-cols-12 ">
+                    <nav className="max-h-screen overflow-y-auto border-b border-gray-200 mb-4 col-span-3 sticky top-0">
+                        <SideMenu items={menu}></SideMenu>
+                    </nav>
+                    <main className="col-span-9 pl-15">
+                        <EstadisticasChart></EstadisticasChart>
+                        <EstadisticasChart></EstadisticasChart>
+                        <EstadisticasChart></EstadisticasChart>
+                        <EstadisticasChart></EstadisticasChart>
+                    </main>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default EstadisticasPage;
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 
-interface MenuItem {
+export interface MenuItem {
     name: string;
     link: string;
     children?: MenuItem[];
