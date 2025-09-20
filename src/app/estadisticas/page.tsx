d0c0344ee4bb0025cd4d286fccfe53031efa95c5
Ivan Flores <ifloresmanc96@gmail.com>
2025-09-19 22:41:55 -0300

    prettier ui for page

--- src/app/estadisticas/page.tsx
+++ src/app/estadisticas/page.tsx
@@ -1,42 +1,64 @@
 import EstadisticasChart from '@/components/reports/EstadisticasChart';
 import { MenuItem, SideMenu } from '@/components/SideMenu';
-import { getNeighborhoods } from '@/lib/mongo-client';
+import { getNeighborhoods, getLatestPriceSurfaceReport } from '@/lib/mongo-client';
+import Introduction from '../../components/reports/Introduccion';
+import PricePerSurface from '../../components/reports/PricePerSurface';
+import { Neighborhood } from '@/models/neighborhood';
+import { AveragePriceSurfaceReportItem } from '@/models/report';
+
+const ChartsContent = ({ neighborhoods, surfacePrices }: { neighborhoods: Neighborhood[]; surfacePrices: AveragePriceSurfaceReportItem[] }) => {
+    return (
+        <>
+            <div id="introduccion">
+                <Introduction />
+            </div>
+            <div id="alquileres" className="mb-12">
+                <EstadisticasChart neighborhoods={neighborhoods}></EstadisticasChart>
+            </div>
+
+            <div id="precios-m2" className="mb-12">
+                <PricePerSurface surfacePrices={surfacePrices} />
+            </div>
+        </>
+    );
+};
 
 const EstadisticasPage = async () => {
     const neighborhoodsFromDb = await getNeighborhoods();
+    const surfacePricesFromDb = await getLatestPriceSurfaceReport();
+
+    const surfacePrices = surfacePricesFromDb ? surfacePricesFromDb.data : [];
     const neighborhoods = neighborhoodsFromDb ? neighborhoodsFromDb.neighborhoods : [];
 
     const menu: MenuItem[] = [
         { name: 'Introducción', link: '#introduccion' },
         {
-            name: 'Alquileres',
+            name: 'Alquileres y expensas',
             link: '#alquileres',
-            children: [
-                { name: 'Progresión histórica', link: '#progresion-historica' },
-                { name: 'Por barrio', link: '#alquileres-por-barrio' },
-                { name: 'Por m²', link: '#alquileres-m2' },
-                { name: 'Por ambientes', link: '#alquileres-ambientes' },
-                { name: 'Con/ sin gastos', link: '#alquileres-gastos' },
-                { name: 'Ranking de barrios', link: '#ranking-barrios' },
-            ],
+        },
+        {
+            name: 'Precios m² de venta',
+            link: '#precios-m2',
         },
         { name: 'Metodología', link: '#metodologia' },
     ];
     return (
         <>
-            <div className="bg-purple-700 text-white p-4 mb-6">
-                <h1 className="text-lg font-bold text-center">Resultados analisis alquileres CABA</h1>
+            <div className=" text-white p-4 mb-6 bg-gradient-to-r from-purple-500 via-purple-800 to-purple-900">
+                <h1 className="text-4xl font-bold text-center ">Resultados analisis alquileres CABA</h1>
             </div>
             <div className="max-w-[1000px] mx-auto ">
-                <div className="grid grid-cols-12 ">
+                <div className="hidden md:grid grid-cols-12 ">
                     <nav className="max-h-screen overflow-y-auto border-b border-gray-200 mb-4 col-span-3 sticky top-0">
                         <SideMenu items={menu}></SideMenu>
                     </nav>
                     <main className="col-span-9 pl-15">
-                        <EstadisticasChart neighborhoods={neighborhoods}></EstadisticasChart>
-                        <EstadisticasChart neighborhoods={neighborhoods}></EstadisticasChart>
-                        <EstadisticasChart neighborhoods={neighborhoods}></EstadisticasChart>
-                        <EstadisticasChart neighborhoods={neighborhoods}></EstadisticasChart>
+                        <ChartsContent neighborhoods={neighborhoods} surfacePrices={surfacePrices} />
+                    </main>
+                </div>
+                <div className="md:hidden px-10">
+                    <main>
+                        <ChartsContent neighborhoods={neighborhoods} surfacePrices={surfacePrices} />
                     </main>
                 </div>
             </div>
--- src/components/reports/EstadisticasChart.tsx
+++ src/components/reports/EstadisticasChart.tsx
@@ -10,7 +10,7 @@ const StackedCheckbox = ({ stacked, setStacked }: { stacked: boolean; setStacked
     return (
         <label className="inline-flex items-center ml-4">
             <input type="checkbox" className="form-checkbox h-3 w-3 text-blue-600" checked={stacked} onChange={() => setStacked(!stacked)} />
-            <span className="ml-2 text-gray-700">Barras apiladas</span>
+            <span className="ml-1 text-gray-700 text-xs">Barras apiladas</span>
         </label>
     );
 };
@@ -45,7 +45,6 @@ const EstadisticasChart: React.FC<{ neighborhoods: MongoNeighborhood[] }> = ({ n
     };
 
     useEffect(() => {
-        console.log('desencadeno');
         if (selectedIds.length > 0) {
             fetchData(selectedIds, filter);
         }
@@ -54,49 +53,55 @@ const EstadisticasChart: React.FC<{ neighborhoods: MongoNeighborhood[] }> = ({ n
     return (
         <div className="w-full">
             <div className="mb-4">
-                <h2 className="text-2xl font-bold text-gray-800 mb-2">Promedios de Alquiler y Expensas por Barrio</h2>
+                <h1 className="text-4xl font-light text-center mb-4 text-balance">Alquiler y Expensas </h1>
                 <p className="text-gray-600">Comparación de costos de vivienda en diferentes barrios de CABA</p>
             </div>
 
-            <RoomFilter onRoomsChange={(rooms) => setFilter({ rooms })} maxRooms={3} />
-            {/* checkbox stacked */}
-            <StackedCheckbox stacked={stacked} setStacked={setStacked} />
-
-            <NeighboursSelector onSelectorChange={handleNeighbourChange} neighbourhoods={neighborhoods.map((n) => ({ id: n.id, name: n.name, selected: false }))} />
+            <div className="grid grid-cols-12 mb-5">
+                <div className="col-span-8">
+                    <NeighboursSelector onSelectorChange={handleNeighbourChange} neighbourhoods={neighborhoods.map((n) => ({ id: n.id, name: n.name, selected: false })).splice(0, 6)} />
+                </div>
+                <div className="flex flex-col col-span-4">
+                    <RoomFilter onRoomsChange={(rooms) => setFilter({ rooms })} maxRooms={3} />
+                    {/* checkbox stacked */}
+                </div>
+            </div>
 
             {chartData.length > 0 && (
-                <ResponsiveContainer width="100%" height={500}>
-                    <BarChart
-                        layout="vertical"
-                        width={500}
-                        height={500}
-                        data={chartData}
-                        margin={{
-                            top: 20,
-                            right: 30,
-                            bottom: 20,
-                            left: 80,
-                        }}
-                    >
-                        <CartesianGrid stroke="#f5f5f5" strokeDasharray="3 3" />
-                        <XAxis type="number" tickFormatter={(value) => `$${value}`} domain={[0, 'dataMax']} />
-                        <YAxis
-                            dataKey="neighborhoodName"
-                            type="category"
-                            scale="band"
-                            width={70}
-                            tick={({ x, y, payload }) => (
-                                <text x={x - 10} y={y + 60} textAnchor="end" fontSize={12}>
-                                    {payload.value}
-                                </text>
-                            )}
-                        />
-                        <Tooltip formatter={(value, name) => [`$${value} ARS`, name === 'alquiler' ? 'Alquiler' : name === 'expensas' ? 'Expensas' : 'Total']} labelFormatter={(label) => `Barrio: ${label}`} />
-                        <Legend formatter={(value) => (value === 'alquiler' ? 'Alquiler promedio' : value === 'expensas' ? 'Expensas promedio' : 'Total')} />
-                        <Bar stackId={stacked ? 'a' : 'b'} dataKey="averagePrice" barSize={20} fill="#413ea0" name="alquiler" />
-                        <Bar stackId="a" dataKey="averageExpense" barSize={20} fill="#ff7300" name="expensas" />
-                    </BarChart>
-                </ResponsiveContainer>
+                <>
+                    <StackedCheckbox stacked={stacked} setStacked={setStacked} />
+                    <ResponsiveContainer width="100%" height={500}>
+                        <BarChart
+                            layout="vertical"
+                            width={500}
+                            height={500}
+                            data={chartData}
+                            margin={{
+                                top: 20,
+                                right: 30,
+                                bottom: 20,
+                                left: 20,
+                            }}
+                        >
+                            <CartesianGrid stroke="#f5f5f5" strokeDasharray="3 3" />
+                            <XAxis type="number" tickFormatter={(value) => `$${value}`} domain={[0, 'dataMax']} />
+                            <YAxis
+                                dataKey="neighborhoodName"
+                                type="category"
+                                width={70}
+                                tick={({ x, y, payload }) => (
+                                    <text x={x - 10} y={y} textAnchor="end" fontSize={12}>
+                                        {payload.value}
+                                    </text>
+                                )}
+                            />
+                            <Tooltip formatter={(value, name) => [`$${value} ARS`, name === 'alquiler' ? 'Alquiler' : name === 'expensas' ? 'Expensas' : 'Total']} labelFormatter={(label) => `Barrio: ${label}`} />
+                            <Legend formatter={(value) => (value === 'alquiler' ? 'Alquiler promedio' : value === 'expensas' ? 'Expensas promedio' : 'Total')} />
+                            <Bar stackId={stacked ? 'a' : 'b'} dataKey="averagePrice" barSize={20} fill="#413ea0" name="alquiler" />
+                            <Bar stackId="a" dataKey="averageExpense" barSize={20} fill="#ff7300" name="expensas" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </>
             )}
         </div>
     );
